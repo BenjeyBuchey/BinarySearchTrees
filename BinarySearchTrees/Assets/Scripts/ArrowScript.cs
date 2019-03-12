bae3c1f73b54940d71dee2d0b49bcb1a7e169c42
BenjeyBuchey <ch0b0@gmx.at>
2019-03-12 19:58:38 +0100

    Node prefab

---
+++ BinarySearchTrees/Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+
+	private Transform fromNode, toNode;
+	private List<Vector2> linePoints = new List<Vector2>();
+
+	// Use this for initialization
+	void Start () {
+		fromNode = gameObject.transform.parent;
+		UpdateLineRenderer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = fromNode.position;
+		//transform.LookAt(toNode);
+		//float mag = (fromNode.position - toNode.position).magnitude;
+		//transform.localScale = new Vector3(mag, mag, 0);
+		UpdateLineRenderer();
+	}
+
+	void UpdateLineRenderer()
+	{
+		if (fromNode == null || toNode == null) return;
+
+		LineRenderer lr = GetComponent<LineRenderer>();
+		//lr.useWorldSpace = true;
+		lr.positionCount = 2;
+		lr.SetPosition(0, fromNode.position);
+		lr.SetPosition(1, toNode.position);
+	}
+}
