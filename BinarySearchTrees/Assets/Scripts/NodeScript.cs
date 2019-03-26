4eb76fd03b90a16cd6259bbc3595640a9a374172
BenjeyBuchey <ch0b0@gmx.at>
2019-03-26 16:53:29 +0100

    fixed adding nodes

---
+++ BinarySearchTrees/Assets/Scripts/NodeScript.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeScript : MonoBehaviour {
+
+	private int level = 0, key = 0;
+	private GameObject parentNode = null, leftNode = null, rightNode = null;
+	// treescript. spawn nodes there.
+
+	public int Level
+	{
+		get
+		{
+			return level;
+		}
+
+		set
+		{
+			level = value;
+		}
+	}
+
+	public int Key
+	{
+		get
+		{
+			return key;
+		}
+
+		set
+		{
+			key = value;
+		}
+	}
+
+	public GameObject ParentNode
+	{
+		get
+		{
+			return parentNode;
+		}
+
+		set
+		{
+			parentNode = value;
+		}
+	}
+
+	public GameObject LeftNode
+	{
+		get
+		{
+			return leftNode;
+		}
+
+		set
+		{
+			leftNode = value;
+		}
+	}
+
+	public GameObject RightNode
+	{
+		get
+		{
+			return rightNode;
+		}
+
+		set
+		{
+			rightNode = value;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		//if (gameObject.transform.parent.gameObject.tag == "Node")
+		//{
+		//	// set parent node
+		//	parentNode = gameObject.transform.parent.gameObject;
+
+		//	// set level
+		//	NodeScript ns = parentNode.GetComponent<NodeScript>();
+		//	level = ns.Level + 1;
+		//}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetChildNodeLeft(GameObject node, int childKey)
+	{
+		leftNode = node;
+		InitChildNode(leftNode, childKey);
+
+		GameObject arrow = gameObject.transform.Find("ArrowLeft").gameObject;
+		if (arrow == null) return;
+		SetArrow(node, arrow);
+	}
+
+	public void SetChildNodeRight(GameObject node, int childKey)
+	{
+		rightNode = node;
+		InitChildNode(rightNode, childKey);
+
+		GameObject arrow = gameObject.transform.Find("ArrowRight").gameObject;
+		if (arrow == null) return;
+		SetArrow(node, arrow);
+	}
+
+	private void InitChildNode(GameObject childNode, int childKey)
+	{
+		// we init child node. this is its parent.
+		// set parentnode of node
+		// set level
+		// set key
+		// set position
+		childNode.GetComponent<NodeScript>().ParentNode = gameObject;
+		childNode.GetComponent<NodeScript>().SetKey(childKey);
+		childNode.GetComponent<NodeScript>().Level = level + 1;
+		childNode.GetComponent<NodeScript>().SetPosition();
+	}
+
+	void SetArrow(GameObject node, GameObject arrow)
+	{
+		if (node == null || arrow == null) return;
+		ArrowScript arrowScript = arrow.GetComponent<ArrowScript>();
+		if (arrowScript == null) return;
+
+		arrowScript.ToNode = node.transform;
+	}
+
+	public void SetPosition()
+	{
+		if(level == 0)
+		{
+			gameObject.transform.localPosition = NodeManager.ROOT_POSITION;
+			return;
+		}
+
+		bool isLeftNode = (parentNode.GetComponent<NodeScript>().leftNode == gameObject) ? true : false;
+
+		float x = (isLeftNode) ? parentNode.transform.localPosition.x - NodeManager.X_DIFF / level : parentNode.transform.localPosition.x + NodeManager.X_DIFF / level;
+		Vector3 position = new Vector3(x, parentNode.transform.localPosition.y - NodeManager.Y_DIFF, 0.0f);
+		gameObject.transform.localPosition = position;
+	}
+
+	public void SetKey(int val)
+	{
+		key = val;
+		SetKeyText();
+	}
+
+	void SetKeyText()
+	{
+		foreach(Text t in gameObject.GetComponentsInChildren<Text>())
+		{
+			if(t.name == "NodeNumber")
+			{
+				t.text = key.ToString();
+			}
+		}
+	}
+}
---
+++ BinarySearchTrees/Assets/Scripts/TreeScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeScript : MonoBehaviour {
+
+	public InputField inputFieldAddNode;
+	public GameObject nodePrefab;
+	private GameObject root = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddNode()
+	{
+		Debug.Log("ADDING: " +inputFieldAddNode.text);
+		int key = -1;
+		if (!int.TryParse(inputFieldAddNode.text, out key)) return;
+		GameObject go = InsertNew(root, false, key, null);
+		//if (root == null)
+		//{
+		//	root = go;
+		//	go.GetComponent<NodeScript>().SetKey(key);
+		//	go.GetComponent<NodeScript>().SetPosition();
+		//}
+	}
+
+	private GameObject Insert(GameObject node, int key)
+	{
+		if (node == null) return SpawnNode(node);
+
+		if (key < node.GetComponent<NodeScript>().Key)
+			node.GetComponent<NodeScript>().SetChildNodeLeft(Insert(node.GetComponent<NodeScript>().LeftNode, key),key);
+		else if (key > node.GetComponent<NodeScript>().Key)
+			node.GetComponent<NodeScript>().SetChildNodeRight(Insert(node.GetComponent<NodeScript>().RightNode, key),key);
+
+		return node;
+	}
+
+	private GameObject SpawnNode(GameObject node)
+	{
+		// spawn new node
+		node = Instantiate(nodePrefab, gameObject.transform);
+		return node;
+	}
+
+	private GameObject InsertNew(GameObject node, bool isLeftNode, int key, GameObject parentNode)
+	{
+		if (node == null) return SpawnNodeNew(node, isLeftNode, key, parentNode);
+
+		if (key < node.GetComponent<NodeScript>().Key)
+			InsertNew(node.GetComponent<NodeScript>().LeftNode, true, key, node);
+		else if (key > node.GetComponent<NodeScript>().Key)
+			InsertNew(node.GetComponent<NodeScript>().RightNode, false, key, node);
+
+		return node;
+	}
+
+	private GameObject SpawnNodeNew(GameObject node, bool isLeftNode, int key, GameObject parentNode)
+	{
+		// spawn new node
+		node = Instantiate(nodePrefab, gameObject.transform);
+
+		if (root == null)
+		{
+			root = node;
+			root.GetComponent<NodeScript>().SetKey(key);
+			root.GetComponent<NodeScript>().SetPosition();
+			return root;
+		}
+
+		if (isLeftNode)
+			parentNode.GetComponent<NodeScript>().SetChildNodeLeft(node, key);
+		else
+			parentNode.GetComponent<NodeScript>().SetChildNodeRight(node, key);
+
+		return node;
+	}
+}
